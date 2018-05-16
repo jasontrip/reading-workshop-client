3e6f8296a4ef56e742756a5aa1e3be57452ac7f0
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-05-16 14:59:56 -0700

    fix warning

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Workshops from './Workshops';
 import Roster from './Roster';
-import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
