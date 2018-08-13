5a92fa6095392fbb2affe5d4e388a53e7329648e
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-08-13 12:11:55 -0700

    move test to testing folder

--- src/components/test/App.test.js
+++ src/components/test/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from '../App';
 import {Provider} from 'react-redux';
 // import store from '../store';
 
