9a1c5ec7b9674131ed11f3f0bf08184debb428a4
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-08-22 20:10:21 -0700

    fix url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === 'production'
-	? 'https://reading-workshop.herokuapp.com'
+	? 'https://reading-workshop.herokuapp.com/api'
 	: 'http://localhost:8080/api';
