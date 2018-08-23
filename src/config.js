7b5c7fed3d1331050965344a51b81d86584b4611
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-08-22 20:03:45 -0700

    take off slash

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === 'production'
-	? 'https://reading-workshop.herokuapp.com/'
+	? 'https://reading-workshop.herokuapp.com'
 	: 'http://localhost:8080/api';
